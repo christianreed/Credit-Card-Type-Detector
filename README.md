5a3a88ed0c724223c44e66f2792147636fb6271c
Christian Reed <staggerwing@gmail.com>
2012-02-27 14:55:30 -0800

    added license and readme

---
+++ README.md
@@ -0,0 +1,36 @@
+jQuery Credit Card Type Detector Plugin
+
+Detail: https://github.com/christianreed/Credit-Card-Type-Detector
+
+Determines possible credit card types as a user types their credit card number.
+
+Intended use is that you start with all possible card logos that you accept and 
+gradually gray them out as the number excludes certain brands of cards. Helps give 
+the user the impression that you know what you're doing and improve conversion.
+
+Designed to work with the cards accepted by Stripe (http://stripe.com), but could
+extended to work with additional card types. Currently detects Visa, Mastercard,
+American Express, Diners Club, Discover, and JCB.
+
+Everything is done with CSS, so as the number changes classes are added to the
+card logo element indicating that a particular card type is possible. Those classes
+are:
+
+* .is_visa
+* .is_mastercard
+* .is_amex
+* .is_diners
+* .is_discover
+* .is_jcb
+
+Also will add a class called .is_nothing if there's no card that matches (useful
+for graying everything out).
+
+Included is a transparent png file with various card types, some css, and a
+possible HTML structure. So pretty much cut and paste, but you could certainly
+build your own structure 
+ 
+Basic Use:
+$('#credit_card_input_field').creditCardTypeDetector('#card_logos_ele');
+
+Default requires no arguments, but looks for the logos to have the class .card_logos
--- jquery.creditCardTypeDetector.js
+++ jquery.creditCardTypeDetector.js
@@ -1,4 +1,40 @@
-// some magic on the form to emphasize the possible card type
+/*!
+ * jQuery Credit Card Type Detector plugin
+ * version 0.9
+ * by Christian Reed - http://christianreed.org
+ * with support from Nick James - http://nickjames.info
+ * for Athletepath - http://athletepath.com
+ * https://github.com/christianreed/Credit-Card-Type-Detector
+ * Copyright (c) 2012 Christian Reed
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * Basic Use:
+ * $('#credit_card_input_field').creditCardTypeDetector('#card_logos_ele');
+ *
+ * Default requires no arguments, but looks for the logos to have the class
+ * .card_logos
+ *
+ * See accompanying files for HTML structure, CSS, and png of card logos.
+ */
+
+
 (function( $ ) {
 	$.fn.creditCardTypeDetector = function( options ) {
 		var settings = $.extend( {
